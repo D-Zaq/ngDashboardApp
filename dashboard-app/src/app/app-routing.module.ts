1c184be5bd1801f12d3ae39b9b26417d6c39cd75
Zakaria Diabi <zakaria.diabi@polymtl.ca>
2021-07-10 19:06:01 -0400

    main page styling

--- dashboard-app/src/app/app-routing.module.ts
+++ dashboard-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { NotesComponent } from './notes/notes.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'favourites', component: FavouritesComponent},
+  {path : 'notes', component: NotesComponent},
+  {path : 'todos', component: TodosComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dashboard-app/src/app/app.module.ts
+++ dashboard-app/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsComponent } from './tabs/tabs.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { TodosComponent } from './todos/todos.component';
+import { NotesComponent } from './notes/notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    FavouritesComponent,
+    TodosComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
